c855f33b4e1fe27ad6e0693064715d7d5e2556f6
EntleD <entledubese92@gmail.com>
2023-04-07 13:45:12 +0200

    Started apiKey and apiUrl from scratch

--- index.js
+++ index.js
@@ -46,44 +46,14 @@ console.log(now.getFullYear);
 let h2 = document.querySelector("h2");
 h2.innerHTML = ` Last updated: ${day} ${hours}:${minutes}`;
 
-function loading(event) {
-  event.preventDefault();
-  document.querySelector("#city-search").innerHTML = response.data.name;
+function searchCity(response) {
+  console.log(response);
 }
 
-function showConditions(response) {
-  let currentTemperature = Math.round(response.data.main.temp);
-  let currentBlow = Math.round(response.data.wind.speed);
-  let description = document.querySelector("#weather-description");
-  let blowElement = document.querySelector("#wind-bar");
-  let mightRain = document.querySelector("#humidity");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = `${currentTemperature}°C`;
-  description.innerHTML = response.data.weather[0].description;
-  blowElement.innerHTML = `Wind: ${currentBlow} km/h`;
-  mightRain.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-}
-
-function searchCity(event) {
-  event.preventDefault();
-  let apiKey = "32b60624683015da61e2ddd35066df2b";
-  let city = document.querySelector("#city-search").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showConditions);
-}
-
-function currentPosition(position) {
-  let apiKey = "32b60624683015da61e2ddd35066df2b";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  let showCurrentPosition = document.querySelector("#latitude-longtitude");
-  showCurrentPosition.innerHTML = `Latitude: ${lat} Longtitude:${lon}`;
-  axios.get(apiUrl).then(showWeather);
-}
+let apiKey = "32b60624683015da61e2ddd35066df2b";
+let city = "Cape Town";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showConditions);
 
 let searchForm = document.querySelector("#search-button");
 searchForm.addEventListener("submit", searchCity);
-
-let cityForm = document.querySelector("#city-form");
-cityForm.addEventListener("submit", loading);
