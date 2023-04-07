18f76607fee01f026b1fad450b6d21839fb6e1f5
EntleD <entledubese92@gmail.com>
2023-04-07 13:52:06 +0200

    Updated apiUrl

--- index.js
+++ index.js
@@ -51,6 +51,5 @@ function showConditions(response) {
 }
 
 let apiKey = "32b60624683015da61e2ddd35066df2b";
-let city = "Cape Town";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cape Town&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showConditions);
