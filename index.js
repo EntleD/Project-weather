1d50b853941586562fa979c286190b6a225f3347
EntleD <entledubese92@gmail.com>
2023-04-08 17:55:46 +0200

    changed city

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ function showConditions(response) {
 }
 
 let apiKey = "32b60624683015da61e2ddd35066df2b";
-let city = "Madrid";
+let city = "Durban";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showConditions);
