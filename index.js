c51be1143571e986681598134c80af3bc7ea2727
EntleD <entledubese92@gmail.com>
2023-04-09 10:04:26 +0200

    added mainCelsius to fahrenheit function

--- index.js
+++ index.js
@@ -54,7 +54,10 @@ function showConditions(response) {
   let blowElement = document.querySelector("#wind-bar");
   let fogElement = document.querySelector("#humidity");
   let mainIcon = document.querySelector("#weather-icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  mainCelsius = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(mainCelsius);
   cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   blowElement.innerHTML = Math.round(response.data.wind.speed);
@@ -79,15 +82,17 @@ function searchCity(event) {
 
 function fahrenheitTemp(event) {
   event.preventDefault();
-  let celsToFah = (17 * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
+  let celsToFah = (mainCelsius * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(celsToFah);
 }
 
-liveCity("Cape Town");
+let mainCelsius = null;
 
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", searchCity);
 
 let fahrenheit = document.querySelector("#fahrenheit-link");
 fahrenheit.addEventListener("click", fahrenheitTemp);
+
+liveCity("Cape Town");
