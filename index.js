bc747b6b71c413009a211f67ccfddbbe1d112995
EntleD <entledubese92@gmail.com>
2023-04-15 17:58:18 +0200

    fixed column of Friday

--- index.js
+++ index.js
@@ -53,11 +53,12 @@ function showForecast() {
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `<div class="col">${day}</div>
+      `
+  <div class="col">${day}</div>
     <div>30°C</div>
-    <div><img src="clipart216360.png" alt="sunny weather" width="50px" /></div>
-    <div>20km/h</div>
-    <div>39%</div></div>
+            <div><img src="clipart216360.png" alt="sunny weather" width="50px" /></div>
+            <div>20km/h</div>
+            <div>39%</div></div>
   </div>
   </div>
 </div>
